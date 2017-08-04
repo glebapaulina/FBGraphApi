33c667405e4afd311bac57a43a7cf84e9bac87a6
Paulina Gleba <paulinagleba@o2.pl>
2017-08-04 14:13:35 +0200

    first steps

---
+++ FBGraphApi/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FBGraphApi
+{
+    class Comment
+    {
+        public string created_time;
+        public string id;
+        public string message;
+        public Comment() { }
+        public Comment(string created_time, string id, string message)
+        {
+            this.created_time = created_time;
+            this.id = id;
+            this.message = message;
+        }
+    }
+}
--- FBGraphApi/MainWindow.xaml.cs
+++ FBGraphApi/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Facebook;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace FBGraphApi
 {
@@ -23,6 +26,47 @@ namespace FBGraphApi
         public MainWindow()
         {
             InitializeComponent();
+
+            FacebookClient client = new FacebookClient();
+            dynamic result = client.Get("oauth/access_token", new
+            {
+                client_id = "...",
+                client_secret = "...",
+                grant_type = "client_credentials"
+            });
+            client.AccessToken = result.access_token;
+
+
+            //retrieving comments for the post
+            var response = (JsonObject)client.Get("1396920153731998/comments");
+            List<Comment> comments = new List<Comment>();
+            foreach (var comm in (JsonArray)response["data"])
+                comments.Add(new Comment
+                {
+                    created_time = (string)(((JsonObject)comm)["created_time"]),
+                    id = (string)(((JsonObject)comm)["id"]),
+                    message = (string)(((JsonObject)comm)["message"])
+                });
+
+            //extracting meesages from comments
+            List<string> messages = new List<string>();
+            foreach (var item in comments)
+            {
+                string json = JsonConvert.SerializeObject(item);
+                Comment deserializedComment = JsonConvert.DeserializeObject<Comment>(json);
+                messages.Add(deserializedComment.message);
+            }
+            
+            
+
+            List<string> match = new List<string>();
+            foreach (string mess in messages)
+            {
+                if (mess.Contains("M"))
+                {
+                    match.Add(mess);
+                }
+            }
         }
     }
 }
